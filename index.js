2a0a65ea2d9fb7f3ca9e9a3b67d47e5ccfc49cd8
Hurricane Hamilton <hhamilto@mtu.edu>
2015-09-28 10:20:05 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,90 @@
+mysql = require('mysql')
+_ = require('lodash')
+deferred = require('deferred')
+
+fs = require('fs')
+util = require('util')
+
+
+whereClauseFromObject = function(obj){
+	return _.map(_.pairs(obj), function(pair){
+		return mysql.escapeId(pair[0]) + '=' + mysql.escape(pair[1])
+	}).join(' AND ')
+}
+
+migrationService = require('./migration-service.js')
+
+var dbReady = false
+
+
+module.exports.initialize =function(conf){
+	if(dbReady) return dbReady
+	dbReady = deferred()
+
+	migrationService.runMigrations(conf).done(function(){
+		var conn = mysql.createConnection(conf.database)
+		var ignoredTables = ['Migrations']
+		module.exports.close = conn.end.bind(conn)
+
+		conn.query('SHOW TABLES', function(err,rows){
+			if(err) {
+				dbReady.reject(err)
+				return
+			}
+			var tables = _.without(_.map(rows,'Tables_in_'+conf.database.database), 'Migrations')
+			deferred.map(tables, function(table){
+				var dfd = deferred()
+				module.exports[table] = {}
+				conn.query('SHOW COLUMNS FROM ' + table, function(err,rows){
+					if(err){
+						dfd.reject(err)
+						return
+					}
+					module.exports[table].columns = rows
+					dfd.resolve()
+				})
+				module.exports[table].create = function(newObj){
+					dfd = deferred()
+					conn.query('INSERT INTO '+table+' SET ?', newObj, function(err, result){
+						if(err)dfd.reject(err)
+						else dfd.resolve(result.insertId)
+					})
+					return dfd.promise
+				}
+				module.exports[table].get = function(obj){
+					dfd = deferred()
+					whereClause = _.keys(obj).length>0?' WHERE '+ whereClauseFromObject(obj):''
+					conn.query('SELECT * FROM '+table+whereClause, function(err, rows){
+						if(err)dfd.reject(err)
+						else dfd.resolve(rows)
+					})
+					return dfd.promise
+					
+				}
+				module.exports[table].set = function(newObj){
+					var id = newObj.id;
+					delete newObj.id;
+					dfd = deferred()
+					conn.query('UPDATE '+table+' SET ? WHERE id=?', [newObj, id], function(err, result){
+						if(err)dfd.reject(err)
+						else dfd.resolve(result.insertId)
+					})
+					return dfd.promise
+				}
+				module.exports[table].remove = function(obj){
+					dfd = deferred()
+					conn.query('DELETE FROM '+table+' WHERE '+whereClauseFromObject(obj), function(err, result){
+						if(err)dfd.reject(err)
+						else dfd.resolve()
+					})
+					return dfd.promise
+				}
+				return dfd
+			}).then(dbReady.resolve,dbReady.reject)
+		})
+		
+	})
+	return dbReady.promise
+}
+
+
---
+++ migration-service.js
@@ -0,0 +1,92 @@
+_ = require('lodash')
+deferred = require('deferred')
+async = require('async')
+
+path = require('path')
+
+//dropAndCreateSql = fs.readFileSync(__dirname+'/dropAndCreate.sql').toString()
+
+module.exports.runMigrations = function(conf){
+	var dfd = deferred()	
+	fs.readdir(conf.migrationDir, function(err, availableMigrations){
+		if(err) {
+			dfd.reject(err)
+			return
+		}
+		var migrationConn = mysql.createConnection(_.defaults({
+			multipleStatements: true
+		},conf.database))
+		createMigrationTableIfNotExist(migrationConn,conf).done(function(){
+			migrationConn.query('SELECT fileName FROM Migrations', function(err, rows){
+				if(err) {
+					dfd.reject(err)
+					return
+				}
+				var appliedMigrations = _.map(rows,'fileName')
+				var unappliedMigrations = _.difference(availableMigrations, appliedMigrations)
+				async.each(unappliedMigrations, function(migrationFileName, cb){
+					fs.readFile(path.join(conf.migrationDir,migrationFileName), function(err, sqlContent){
+						if(err) cb(err)
+						else migrationConn.query(sqlContent.toString(), function(err){
+							if(err) cb(err)
+							else migrationConn.query('INSERT INTO Migrations (fileName) VALUES (?)', migrationFileName, function(err){
+								if(err) cb(err)
+								else cb()
+							})
+						})
+					})
+				}, function(err){
+					if (err) dfd.reject(err)
+					else migrationConn.end(function(err){ 
+						if (err) dfd.reject(err)
+						else dfd.resolve()
+					})
+				})
+			})
+		})
+	})
+
+	
+	return dfd.promise
+}
+
+createMigrationTableIfNotExist = function(migrationConn,conf){
+	var dfd = deferred()
+		migrationConn.query('SHOW TABLES', function(err, results) {
+			if (err) {
+				dfd.reject(err)
+				return
+			}
+			var filterObj = {}
+			filterObj['Tables_in_'+conf.database.database] = 'Migrations'
+			if(_.filter(results, filterObj).length == 0){
+				console.log(path.join(__dirname,'createMigrationTable.sql'))
+				fs.readFile(path.join(__dirname,'createMigrationTable.sql'), function(err, createMigrationTableSql){
+					if (err) {
+						dfd.reject(err)
+						return
+					}
+					migrationConn.query(createMigrationTableSql.toString(), function(err, results) {
+						if (err) dfd.reject(err)
+						else dfd.resolve()
+					})
+				})
+				
+			}else{
+				dfd.resolve()
+			}
+		})	
+	
+
+	return dfd.promise
+}
+
+module.exports.dropAndCreate = function(){
+	var dfd = deferred()
+	migrationConn.query(dropAndCreateSql, function(err, results) {
+		if (err) dfd.reject(err)
+		else dfd.resolve()
+	})
+	return dfd.promise
+}
+
